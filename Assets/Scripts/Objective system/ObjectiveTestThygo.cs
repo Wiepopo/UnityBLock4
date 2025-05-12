2849af6690fda70a99b017ee2524cdd85f263b72
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-05-12 15:37:27 +0200

    Customizable text on trigger basis
    you can put this script on a trigger and adjust what the script will say in the editor not hardcoding

---
+++ Assets/Scripts/Objective system/ObjectiveTestThygo.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveTestThygo : MonoBehaviour
+{
+    [SerializeField] string missionObjectiveText = "Test message";
+
+    //Public Variables //
+    public AudioSource objFX;
+    public GameObject theobjective;
+    public GameObject Objectivetrigger;
+    public GameObject theText;
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        StartCoroutine(missionObj());
+        Debug.Log("it triggers");
+
+    }
+
+    private IEnumerator missionObj()
+    {
+        objFX.Play();
+        theText.SetActive(true);
+        theText.GetComponent<Text>().text = missionObjectiveText;
+        yield return null;
+       
+    }
+
+
+}
+
