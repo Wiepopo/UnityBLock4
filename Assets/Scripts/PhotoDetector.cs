177976fca689940ae5192139d2d780b9ce075ccc
FoxxMa4ka <kamenemnqk@gmail.com>
2025-05-15 23:22:42 +0200

    Raycast recognition (evidence check)

---
+++ Assets/Scripts/PhotoDetector.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PhotoDetector : MonoBehaviour
+{
+    [SerializeField] private Camera photoCamera;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0)) // Replace with your photo key
+        {
+            TryDetectEvidence();
+        }
+    }
+
+    public void TryDetectEvidence()
+    {
+        Ray ray = photoCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(ray, out RaycastHit hit, 10f))
+        {
+            if (hit.collider.CompareTag("Evidence"))
+            {
+                Debug.Log("Evidence found: " + hit.collider.name);
+            }
+            else
+            {
+                Debug.Log("Hit something, but it's not evidence.");
+            }
+        }
+        
+    }
+}
