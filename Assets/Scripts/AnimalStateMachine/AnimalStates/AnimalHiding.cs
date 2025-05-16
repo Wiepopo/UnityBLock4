194a58b6c52a669be5354f868e1b90c6f6613361
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-05-16 11:42:29 +0200

    Start of the animal Statemachine
    lot of scripts for different states of animals and the statemanager
    //not finished

---
+++ Assets/Scripts/AnimalStateMachine/AnimalStates/AnimalHiding.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AnimalHiding : AnimalBaseState
+{
+    public override void EnterState(AnimalStateManager animal)
+    {
+
+    }
+    public override void UpdateState(AnimalStateManager animal)
+    {
+
+    }
+    public override void OnCollisionEnter(AnimalStateManager animal)
+    {
+
+    }
+}
