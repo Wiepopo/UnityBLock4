2a586993915655ae1d8cf4d3ddd9a73fe76badd6
FoxxMa4ka <kamenemnqk@gmail.com>
2025-05-19 00:31:27 +0200

    outline

--- Assets/Scripts/OutlineSelection.cs
+++ Assets/Scripts/OutlineSelection.cs
@@ -1,39 +1,61 @@
-using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class OutlineSelection : MonoBehaviour
 {
-    private Transform highlight;
-    private RaycastHit raycastHit;
+    private Transform highlightedGroup;
+    private List<Outline> currentOutlines = new List<Outline>();
+
+    // You can tweak this to control the outline thickness
+    private float outlineWidth = 15f;
+    private Color outlineColor = Color.red;
 
     void Update()
     {
-        // Remove previous highlight
-        if (highlight != null)
+        // Disable previous outlines
+        if (highlightedGroup != null)
         {
-            var oldOutline = highlight.GetComponent<Outline>();
-            if (oldOutline != null)
-                oldOutline.enabled = false;
-            highlight = null;
+            foreach (var outline in currentOutlines)
+            {
+                if (outline != null)
+                    outline.enabled = false;
+            }
+            currentOutlines.Clear();
+            highlightedGroup = null;
         }
 
+        // Raycast from mouse to world
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit))
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out RaycastHit hit))
         {
-            Transform candidate = raycastHit.transform;
-            if (candidate.CompareTag("Selectable"))
+            Transform target = hit.transform;
+
+            // Look for a parent with tag "Selectable"
+            Transform group = target.CompareTag("Selectable") ? target : target.GetComponentInParent<Transform>();
+            if (group != null && group.CompareTag("Selectable"))
             {
-                Outline outline = candidate.GetComponent<Outline>();
-                if (outline == null)
+                Renderer[] renderers = group.GetComponentsInChildren<Renderer>();
+                foreach (Renderer rend in renderers)
                 {
-                    outline = candidate.gameObject.AddComponent<Outline>();
-                    outline.OutlineColor = Color.magenta;
-                    outline.OutlineWidth = 7.0f;
+                    Outline outline = rend.GetComponent<Outline>();
+                    if (outline == null)
+                    {
+                        outline = rend.gameObject.AddComponent<Outline>();
+                        outline.OutlineMode = Outline.Mode.OutlineAll;
+                        outline.OutlineColor = outlineColor;
+                        outline.OutlineWidth = outlineWidth;
+                    }
+
+                    outline.OutlineMode = Outline.Mode.OutlineAll;
+                    outline.OutlineColor = outlineColor;
+                    outline.OutlineWidth = outlineWidth;
+                    outline.enabled = true;
+
+                    currentOutlines.Add(outline);
                 }
 
-                outline.enabled = true;
-                highlight = candidate;
+                highlightedGroup = group;
             }
         }
     }
