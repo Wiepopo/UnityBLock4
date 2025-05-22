323c6dad6826f3ddb0309f61d5523e3566311461
mrLOEPSOEP <tm.lansbergen@outlook.com>
2025-05-22 12:26:58 +0200

    Animal AI no keys
    removed manual activation to change between states now works on its own

--- Assets/Scripts/AnimalAI/AnimalAImanager.cs
+++ Assets/Scripts/AnimalAI/AnimalAImanager.cs
@@ -5,31 +5,48 @@ public class AnimalAImanager : MonoBehaviour
 {
     private AnimalAIWander animalAIWander;
     private AnimalAIIdle animalAIIdle;
+    private AnimalAIRunning animalAIRunning;
 
     //Statements
-    bool switchmode = false;
-    
+    bool switchmodeactive = false;
+
+    //Different timers for switching between states\
+    [SerializeField] float wanderTime;
+    [SerializeField] float runTime;
+    [SerializeField] float idleTime;
+    [SerializeField] float switchToNextStateTimer;
+    private float currentTime;
+
     void Awake()
     {
         animalAIWander = GetComponent<AnimalAIWander>();
         animalAIIdle = GetComponent<AnimalAIIdle>();
+        animalAIRunning = GetComponent<AnimalAIRunning>();
+        
     }
 
     void Start()
     {
         animalAIWander.enabled = false;
+        animalAIRunning.enabled = false;
+        currentTime = switchToNextStateTimer;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
+        if (currentTime > 0f)
         {
-            switchmode = !switchmode;
+            currentTime -= Time.deltaTime;
         }
-       // Debug.Log(switchmode);
+
+        if (currentTime <= 0f)
+            {
+                switchmodeactive = !switchmodeactive;
+                currentTime = wanderTime;
+            }
         
-            animalAIIdle.enabled = !switchmode;
-            animalAIWander.enabled = switchmode;
+            animalAIIdle.enabled = !switchmodeactive;
+            animalAIWander.enabled = switchmodeactive;
         
        
     }
